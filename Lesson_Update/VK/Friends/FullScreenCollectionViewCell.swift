c7ebe2d4d8d56637dd257a587019a481f8d900a5
Sergey-Zaitsev <73549064+Sergey-Zaitsev@users.noreply.github.com>
2021-02-09 02:34:47 +0300

    Add

---
+++ Lesson_Update/VK/Friends/FullScreenCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenCollectionViewCell.swift
+//  VK
+//
+//  Created by Сергей Зайцев on 09.02.2021.
+//
+
+import UIKit
+
+class FullScreenCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
